d68a51a14b90d3eeb07417d438a1729f9fad9cf9
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-12-22 23:57:14 +0100

    OSM entry map added

---
+++ RadioSimulator/RadioConsole.Web/wwwroot/js/entryMap.js
@@ -0,0 +1,12 @@
+﻿    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([19.944544, 50.049683]),
+            zoom: 12
+        })
+    });
