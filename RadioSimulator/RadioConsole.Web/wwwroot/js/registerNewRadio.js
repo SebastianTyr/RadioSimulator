ba177dfeb5ea997a7ad98ae915bceea501fc5b08
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-12-22 23:44:50 +0100

    fixed ajax validation and home page changed

--- RadioSimulator/RadioConsole.Web/wwwroot/js/registerNewRadio.js
+++ RadioSimulator/RadioConsole.Web/wwwroot/js/registerNewRadio.js
@@ -1,30 +1,17 @@
-﻿document.forms[0].onsubmit = () => {
-    let formData = new FormData(document.forms[0]);
-    let alertEl = document.getElementById('success-alert');
-    let errorEl = document.getElementById('error-alert');
-    fetch('/Radio/Register', {
-        method: 'POST',
-        body: new URLSearchParams(formData)
-    })
-        .then(() => {
-            alertEl.style.display = 'flex';
+﻿document.forms[0].onsubmit = function() {
+    if ($(this).valid()) {
+        let formData = new FormData(document.forms[0]);
+        let alertEl = document.getElementById('success-alert');
+        fetch('/Radio/Register', {
+            method: 'POST',
+            body: new URLSearchParams(formData)
         })
-        .catch(() => {
-            errorEl.style.display = 'flex';
-        });
+            .then(() => {
+                alertEl.style.display = 'flex';
+            });
+    } else {
+        let errorEl = document.getElementById('error-alert')
+        errorEl.style.display = 'flex';
+    }
     return false;
 };
-//$("#formdata").submit(function (e) {
-//    let alertEl = $('success-alert');
-//    let errorEl = $('error-alert');
-//    e.preventDefault();
-//    var form = $(this);
-//    $.ajax(
-//        {
-//            type: "POST",
-//            url: "/Radio/Register",
-//            data: form.serialize(),
-//            success: () => { alertEl.style.display = 'flex'; },
-//            error: () => { errorEl.style.display = 'flex'; }
-//        });
-//});
